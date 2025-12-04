775f31846e7ae6bacdd39a214aa8496ac30cc4dd
RicardoFernandes99 <filipericardo7@hotmail.com>
2025-12-04 11:35:33 +0000

    ..

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -18,7 +18,7 @@ export default function LoginPage() {
     setError(null);
 
     try {
-      const res = await fetch("http://localhost:3000/api/users/login", {
+      const res = await fetch("http://localhost:3000/api/auth/login", {
         method: "POST",
         headers: {"Content-Type": "application/json"},
         body: JSON.stringify({email, password}),
